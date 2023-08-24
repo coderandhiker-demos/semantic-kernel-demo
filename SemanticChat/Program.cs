72784aa7739d3dc3a3e7c1c5821237a08cf2d37f
Chris Roberts <chrisrt@microsoft.com>
2023-08-24 17:04:59 -0500

    Walking tours variant

--- SemanticChat/Program.cs
+++ SemanticChat/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.SemanticKernel.SemanticFunctions;
 using System;
 using System.Threading.Tasks;
 using Spectre.Console;
+using Microsoft.SemanticKernel.Orchestration;
+using System.Diagnostics;
 
 namespace SemanticChat
 {
@@ -17,66 +19,36 @@ namespace SemanticChat
 
             IKernel kernel = builder.Build();
 
-            const string skPrompt = @"
-You are Kevin, a helpful bot that writes using concise business language, adopted from the 
-HBR Guide to Better Business Writing. You will answer questions, provide instruction
-or answer 'I do not know' if you do not have an answer. You will be very concise and avoid 
-overly verbose language.
-
-{{$history}}
-User: {{$userInput}}
-Kevin:";
+            string skPrompt = File.ReadAllText("Prompt.txt");
 
             var promptTemplate = new PromptTemplate(skPrompt, Settings.GetDefaultConfig(), kernel);
             var functionConfig = new SemanticFunctionConfig(Settings.GetDefaultConfig(), promptTemplate);
             var chatFunction = kernel.RegisterSemanticFunction("ChatBot", "Chat", functionConfig);
 
             var context = kernel.CreateNewContext();
+            Console.Write("Enter city name: ");
+            var city = Console.ReadLine();
+            var outputFile = @"C:\temp\sample.html";
+            context.Variables["userInput"] = city;
 
-            // Initialize a chat history for this context
-            var history = "";
-            context.Variables["history"] = history;
+            var bot_answer = await chatFunction.InvokeAsync(context);
+            var answer = bot_answer.ToString().Replace("```html", "").Replace("```", "");
 
-            while (true) // Infinite loop until "exit" command
+            if (answer.Contains("<h2"))
             {
-                // Display chat history in a yellow box
-                if (!string.IsNullOrEmpty(history))
-                {
-                    AnsiConsole.Render(
-                        new Panel("[yellow]" + history.Trim() + "[/]")
-                        .Header("[yellow]Context - Chat History[/]")
-                        .BorderColor(Color.Yellow)
-                    );
-                    AnsiConsole.WriteLine();
-                }
+                var template = File.ReadAllText("Template.html")
+                .Replace("{{BODY}}", answer.ToString())
+                .Replace("{{LOCATION}}", city);
+                File.WriteAllText(outputFile, template);
 
-                // Display prompt with cyan color for user input
-                AnsiConsole.Markup("[cyan]User: [/]");
-                var userInput = Console.ReadLine();
 
-                // Exit the loop if the user types "exit"
-                if (userInput?.ToLower().Trim() == "exit")
-                    break;
+                Console.WriteLine(bot_answer);
 
-                context.Variables["userInput"] = userInput;
-
-                var bot_answer = await chatFunction.InvokeAsync(context);
-
-                // Add a horizontal line if there's existing history
-                if (!string.IsNullOrEmpty(history))
-                {
-                    history += "[yellow]──────[/]\n";
-                }
-                history += $"\nUser: {userInput}\nKevin: {bot_answer.Result}\n";
-                context.Variables.Update(history);
-
-                var fullResponse = bot_answer.Result;
-                var kevinResponseStart = fullResponse.LastIndexOf("Kevin: ") + 7;  // +7 to account for the length of "Kevin: "
-                var kevinResponse = fullResponse.Substring(kevinResponseStart).Trim();
-                Console.WriteLine("");
-
-                // Display Kevin's response with dark orange color
-                AnsiConsole.MarkupLine("[darkorange]Kevin: {0}\n[/]", kevinResponse);
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {outputFile}") { CreateNoWindow = true });
+            }
+            else
+            {
+                Console.WriteLine("No data.");
             }
         }
     }
